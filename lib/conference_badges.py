b451c513b70dae55dbd9c2a2d1b1d06f4a413412
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-13 16:49:58 -0500

    badges and rooms

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,21 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    batch = []
+    for name in names:
+        batch.append(f"Hello, my name is {name}.")
+    return batch;
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for index, name in enumerate(names):
+        room_number = index + 1
+        rooms.append(f"Hello, {name}! You'll be assigned to room {room_number}!")
+    return rooms
 
 def printer(names):
-    return None
+    name = batch_badge_creator(names)
+    room = assign_rooms(names)
+    for person in name + room:
+        print(person)
